39590b7380d7e3a13bb465b4d9596177012f92cd
StevenDessasore <stevendessasoreba@gmail.com>
2021-07-13 21:39:28 -0400

    commit tree 0e279e8b0e9af72f85f5d32218d9f7ba24833bce

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Steven", 18, "M", "Red", "Cool", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Demeris", 28, "F", "Blue", "Mean", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Cameron", 10, "M", "Red", "Nice", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Amare", 4, "M", "Green", "Funny", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Maddox", 4, "M", "Blue", "Sweet", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sharesa", 2, "F", "Red", "Rude", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Winter", 18, "F", "White", "Active", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 16, "F", "White", "Cool", 0);
